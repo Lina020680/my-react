9d959e8ed9b9cc077036cc51b23449dfcefd6ee3
lina <lina.parts@googlemail.com>
2024-04-28 06:10:38 +0200

    Consultation01

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
-import Homework02 from './homeworks/Homework02/Homework02';
+import Consultation01 from './consultation/consultation01/consultation';
+//import Homework02 from './homeworks/Homework02/Homework02';
+import Lesson03 from './lessons/Lessons03/Lessons03';
 
 function App() {
   return (
     <div className="App">
-      <Homework02/>
+      {/*<Homework02/>*/}
+      {/*<Lesson03 />*/}
+      <Consultation01/>
     </div>
   );
 }
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+function Button({ buttonName = "Get data", isPrimaryButton }) {
+  // 1 вариант
+    //const buttonClass = isPrimaryButton
+    //? "main-button primary-button"
+    //: "main-button secondary-button";
+
+    // 2 вариант
+    const buttonClass = `main-button ${isPrimaryButton
+    ? "primary-button" : "secondary-button"}`;
+
+  return <button className={buttonClass}>{buttonName}</button>;
+}
+
+export default Button;
