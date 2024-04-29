319e1461f17db1a655050047aed00ca621d82514
lina <lina.parts@googlemail.com>
2024-04-29 04:03:24 +0200

    Lesson04

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css';
-import Consultation01 from './consultation/consultation01/consultation';
+//import Homework03 from './homeworks/Homework03/Homework03';
+//import Consultation01 from './consultation/consultation01/consultation';
 //import Homework02 from './homeworks/Homework02/Homework02';
-import Lesson03 from './lessons/Lessons03/Lessons03';
+//import Lesson03 from './lessons/Lessons03/Lessons03';
+import Lesson04 from './lessons/Lesson04/Lesson04';
 
 function App() {
   return (
     <div className="App">
       {/*<Homework02/>*/}
       {/*<Lesson03 />*/}
-      <Consultation01/>
+      {/*<Consultation01/>*/}
+      {/*<Homework03/>*/}
+      <Lesson04/>
     </div>
   );
 }
